fbd05621fe1b223c872f5c7171112c1e185d0ec1
Mortol <94522772+Mortol2003@users.noreply.github.com>
2023-11-08 04:42:22 +0300

    last update

--- app/src/main/java/com/topic2/android/notes/util/Note.kt
+++ app/src/main/java/com/topic2/android/notes/util/Note.kt
@@ -7,6 +7,9 @@ import androidx.compose.material.Checkbox
 import androidx.compose.ui.Alignment
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -31,7 +34,8 @@ fun Note() {
             .background(Color.White, backgroundShape)
     ) {
         NoteColor(
-            modifier = Modifier.align(Alignment.CenterVertically),
+            modifier = Modifier.align(Alignment.CenterVertically)
+                .padding(start = 16.dp, end = 16.dp),
             color = rwGreen,
             size = 40.dp,
             padding = 4.dp,
@@ -52,8 +56,16 @@ fun Note() {
             )
             Text(
                 text = stringResource(id = R.string.content),
-                maxLines = 1
+                color = Color.Black,
+                maxLines = 1,
+                style = TextStyle(
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    letterSpacing = 0.15.sp
+                )
             )
+
+
         }
         Checkbox(
             checked = false,
